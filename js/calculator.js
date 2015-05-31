41ad2904c638b24ea15aa38e400118bfbc9cb3a3
Constanti <constantin.dumba@gmail.com>
2015-05-30 19:39:40 -1000

    fixed a bug that wouldnt allow withdrawing of money

--- js/calculator.js
+++ js/calculator.js
@@ -55,7 +55,7 @@ var calculatorModule = (function(){
     withdraw : function(){
       if(memory >= display){
         memory -= display;
-      } else display = 'not enough funds'
+      }
 
     }
   }
@@ -560,11 +560,15 @@ document.getElementById("depositCash").addEventListener('click',function(){
   document.getElementById('display').innerHTML = 'deposited' + ' ' + '$'+display;
 });
 document.getElementById("withdrawCash").addEventListener('click',function(){
-  calculatorModule.saveMemory();
-  document.getElementById('display').innerHTML = 'deposited' + ' ' + '$'+display;
+  var previousBalance = calculatorModule.recallMemory();
+  calculatorModule.withdraw();
+  if(calculatorModule.recallMemory() === calculatorModule.recallMemory() && display !== 0){
+    document.getElementById('display').innerHTML = 'The request exceeds your balance'
+  } else
+  document.getElementById('display').innerHTML = 'withdrawing' + ' ' + '$'+display;
 });
 document.getElementById("getBalance").addEventListener('click',function(){
-  document.getElementById('display').innerHTML = 'you have' + ' $'+calculatorModule.recallMemory();
+  document.getElementById('display').innerHTML = 'you have' + ' $'+ calculatorModule.recallMemory();
 });
 
 
